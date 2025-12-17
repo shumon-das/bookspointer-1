2a30da717f94d951bc17dfbf728067eafeca234f
shumon <shumonsb@gmail.com>
2025-12-17 12:52:07 +0600

    fixed title and details screen

--- app/screens/user/userProfile.tsx
+++ app/screens/user/userProfile.tsx
@@ -1,13 +1,25 @@
 import { router, useNavigation } from 'expo-router';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { BackHandler, SafeAreaView, View } from 'react-native';
 import UserProfileHeader from '@/components/micro/user/profile/UserProfileHeader';
 import { ActivityIndicator } from 'react-native-paper';
 import { useAuthStore } from '@/app/store/auth';
 import UserProfileTabView from '@/components/micro/user/profile/UserProfileTabView';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import UserSeries from '@/components/micro/user/UserSeries';
 
 const userProfile = () => {
   const { user: author, authenticatedUser } = useAuthStore();
+  const [user, setUser] = useState(authenticatedUser as any)
+
+  useEffect(() => {
+      const loadLoggedInUser = async () => {
+        const storedUser = await AsyncStorage.getItem('auth-user');
+        setUser(storedUser ? JSON.parse(storedUser) : null);
+    }
+
+    loadLoggedInUser()
+  },[author])
         
   const navigation = useNavigation()
   useEffect(() => navigation.setOptions({ headerShown: false }), []);
@@ -33,7 +45,10 @@ const userProfile = () => {
   return (
     <SafeAreaView style={{flex: 1}}>
       <UserProfileHeader author={author} />  
-      <UserProfileTabView onSelectSeries={onSelectSeries} />
+      {user && author && user.id === author.id 
+        ? (<UserProfileTabView onSelectSeries={onSelectSeries} />)
+        : <UserSeries series={author.series} author={author} onChooseSeries={(seriesName: string) => onSelectSeries(seriesName)} />
+      }
     </SafeAreaView>
   )
 }
--- components/micro/book/details/DetailsOffline.tsx
+++ components/micro/book/details/DetailsOffline.tsx
@@ -2,7 +2,7 @@ import labels from "@/app/utils/labels"
 import { View, Text } from "react-native"
 
 const DetailsOffline = () => {
-    return (<View>
+    return (<View style={{height: '99%', flex: 1, justifyContent: 'center', alignItems: 'center',marginVertical: 5}}>
         <Text>{labels.detailsScreenOfflineMessage}</Text>
     </View>)
 }
